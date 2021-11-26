09a2810937c6c78149e8ad07bd9167e95f0a17c8
Alexander Sokolov <sashkacosmonaut@gmail.com>
2021-11-27 02:48:49 +0300

    Добавлен тестовый модуль обработки команд движения корабля.

---
+++ NTOJSpaceDiscordBot/NTOJSpaceDiscordBot/Modules/MovementModule.cs
@@ -0,0 +1,37 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace NTOJSpaceDiscordBot.Modules
+{
+    /// <summary>
+    /// Модуль обработки сигналов для движения корабля.
+    /// Modules must be public and inherit from an IModuleBase.
+    /// </summary>
+    public class MovementModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("forward")]
+        [Alias("Forward", "FORWARD")]
+        public async Task Forward()
+        {
+            await ReplyAsync($"{Context.User} двигается вперёд");
+        }
+
+        [Command("back")]
+        public async Task Back()
+        {
+            await ReplyAsync($"{Context.User} двигается назад");
+        }
+
+        [Command("left")]
+        public async Task Left()
+        {
+            await ReplyAsync($"{Context.User} двигается влево");
+        }
+
+        [Command("right")]
+        public async Task Right()
+        {
+            await ReplyAsync($"{Context.User} двигается вправо");
+        }
+    }
+}
