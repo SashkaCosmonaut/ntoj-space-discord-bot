4172dd4cbfdb241cfcd6ccf1f018c36957400f83
Alexander Sokolov <sashkacosmonaut@gmail.com>
2021-11-27 11:03:10 +0300

    Добавлены настройки сервисов.

--- NTOJSpaceDiscordBot/NTOJSpaceDiscordBot/Program.cs
+++ NTOJSpaceDiscordBot/NTOJSpaceDiscordBot/Program.cs
@@ -16,6 +16,8 @@ namespace NTOJSpaceDiscordBot
     {
         /// <summary>
         /// Главная функция программы.
+        /// Call the Program constructor, followed by the 
+        /// MainAsync method and wait until it finishes (which should be never).
         /// </summary>
         public static void Main() 
             => new Program().MainAsync().GetAwaiter().GetResult();
@@ -37,8 +39,9 @@ namespace NTOJSpaceDiscordBot
 
             var client = services.GetRequiredService<DiscordSocketClient>();
             var commandService = services.GetRequiredService<CommandService>();
-            var logger = services.GetRequiredService<LoggingService>();
-            
+
+            await services.GetRequiredService<LoggingService>().InitializeAsync();
+
             // Tokens should be considered secret data and never hard-coded.
             // We can read from the environment variable to avoid hard coding.
             await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("NTOJSpaceBotToken"));
@@ -52,15 +55,44 @@ namespace NTOJSpaceDiscordBot
 
         /// <summary>
         /// Подключаем сервисы, используем инверсию зависимостей.
+        /// Setup your DI container.
         /// </summary>
         /// <returns>Провайдер сервисов, который осдержит все подключенные сервисы.</returns>
         private ServiceProvider ConfigureServices()
         {
+            var discordSocketConfig = new DiscordSocketConfig
+            {
+                // How much logging do you want to see?
+                LogLevel = LogSeverity.Info,
+
+                // If you or another service needs to do anything with messages
+                // (eg. checking Reactions, checking the content of edited/deleted messages),
+                // you must set the MessageCacheSize. You may adjust the number as needed.
+                // MessageCacheSize = 50,
+            };
+
+            var commandServiceConfig = new CommandServiceConfig
+            {
+                // Again, log level:
+                LogLevel = LogSeverity.Info,
+
+                // There's a few more properties you can set,
+                // for example, case-insensitive commands.
+                CaseSensitiveCommands = false                
+            };
+
             return new ServiceCollection()
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton<CommandService>()
+
+                // Repeat this for all the service classes
+                // and other dependencies that your commands might need.
+                .AddSingleton(new DiscordSocketClient(discordSocketConfig))
+                .AddSingleton(new CommandService(commandServiceConfig))
                 .AddSingleton<LoggingService>()
                 .AddSingleton<CommandHandlingService>()
+
+                // When all your required services are in the collection, build the container.
+                // Tip: There's an overload taking in a 'validateScopes' bool to make sure
+                // you haven't made any mistakes in your dependency graph.
                 .BuildServiceProvider();
         }
     }
