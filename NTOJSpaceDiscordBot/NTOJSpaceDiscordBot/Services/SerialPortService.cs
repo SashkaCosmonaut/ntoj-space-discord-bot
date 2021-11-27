8d30e336d52fb362ec8d65d2280cd11ead2eb149
Alexander Sokolov <sashkacosmonaut@gmail.com>
2021-11-27 11:37:32 +0300

    Создан класс сервиса для взаимодействия с устройствами по последовательному порту.

---
+++ NTOJSpaceDiscordBot/NTOJSpaceDiscordBot/Services/SerialPortService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading.Tasks;
+
+namespace NTOJSpaceDiscordBot.Services
+{
+    /// <summary>
+    /// Сервис для взаимодействия с устройствами по последовательному порту.
+    /// </summary>
+    public class SerialPortService
+    {
+        /// <summary>
+        /// Контейнер сервисов.
+        /// </summary>
+        private readonly IServiceProvider _services;
+
+        /// <summary>
+        /// Объект последовательного порта.
+        /// </summary>
+        private readonly SerialPort _serialPort;
+
+        /// <summary>
+        /// Конструктор сервиса.
+        /// </summary>
+        /// <param name="services">Сохраняем контейнер сервисов.</param>
+        public SerialPortService(IServiceProvider services)
+        {
+            _services = services;
+        }
+
+        /// <summary>
+        /// Инициализация сервиса.
+        /// </summary>
+        /// <returns>Асинхронная операция.</returns>
+        public async Task InitializeAsync()
+        {
+
+        }
+    }
+}
