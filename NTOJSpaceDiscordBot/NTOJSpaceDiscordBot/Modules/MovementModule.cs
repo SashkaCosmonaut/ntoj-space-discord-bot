af45091dc77c8ccddb1180385c89d4b33707eed1
Alexander Sokolov <sashkacosmonaut@gmail.com>
2021-11-27 12:50:08 +0300

    Используется метод отправки сообщения движения вперёд.

--- NTOJSpaceDiscordBot/NTOJSpaceDiscordBot/Modules/MovementModule.cs
+++ NTOJSpaceDiscordBot/NTOJSpaceDiscordBot/Modules/MovementModule.cs
@@ -1,4 +1,7 @@
 ﻿using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using NTOJSpaceDiscordBot.Services;
+using System;
 using System.Threading.Tasks;
 
 namespace NTOJSpaceDiscordBot.Modules
@@ -9,10 +12,26 @@ namespace NTOJSpaceDiscordBot.Modules
     /// </summary>
     public class MovementModule : ModuleBase<SocketCommandContext>
     {
+        /// <summary>
+        /// Сервис последовательного порта.
+        /// </summary>
+        private readonly SerialPortService _serialPortService;
+
+        /// <summary>
+        /// Конструктор модуля.
+        /// </summary>
+        /// <param name="services">Контейнер сервисов.</param>
+        public MovementModule(IServiceProvider services)
+        {
+            _serialPortService = services.GetRequiredService<SerialPortService>();
+        }
+
         [Command("forward")]
         public async Task Forward()
         {
-            await ReplyAsync($"{Context.User} двигается вперёд");
+            var result = await _serialPortService.SendForwardAsync();
+
+            await ReplyAsync($"{Context.User} двигается вперёд: {result}");
         }
 
         [Command("back")]
