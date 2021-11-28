5339fbbd180ad5f3c3b1e2d7953cb3ac1a981bed
Alexander Sokolov <sashkacosmonaut@gmail.com>
2021-11-28 13:13:41 +0300

    Убрано использование захардкоженных команд.

--- ArduinoIRController/ArduinoIRController.ino
+++ ArduinoIRController/ArduinoIRController.ino
@@ -40,16 +40,15 @@ int command;          // Расшифрованная команда для ко
 int decodeCommandChar(char commandChar)
 {
   switch (commandChar) {
-    case FORWARD_STR: return P2;
-    case LEFT_STR:    return P4;
-    case STOP_STR:    return P5;
-    case RIGHT_STR:   return P6;
-    case BACK_STR:    return P8;
-    
     case '0': return P0;
     case '1': return P1;
+    case '2': return P2;
     case '3': return P3;
+    case '4': return P4;
+    case '5': return P5;
+    case '6': return P6;
     case '7': return P7;
+    case '8': return P8;
     case '9': return P9;
     case '*': return P_STAR;
     case '#': return P_HASH;
