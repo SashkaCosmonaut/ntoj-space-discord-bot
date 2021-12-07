147cbb42ab408ecf8154bbe169a93757b78353ff
Alexander Sokolov <sashkacosmonaut@gmail.com>
2021-12-07 14:05:28 +0300

    Добавлен класс для конфигурационного файла программы.

---
+++ NTOJSpaceDiscordBot/NTOJSpaceDiscordBot/ProgramConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace NTOJSpaceDiscordBot
+{
+    /// <summary>
+    /// Класс для конфигурационного файла программы.
+    /// </summary>
+    public class ProgramConfig
+    {
+        /// <summary>
+        /// Название порта.
+        /// </summary>
+        public string PortName { get; set; }
+
+        /// <summary>
+        /// Скорость передачи порта.
+        /// </summary>
+        public int BaudRate { get; set; }
+        
+        /// <summary>
+        /// Время ожидания чтения с порта.
+        /// </summary>
+        public int ReadTimeout { get; set; }
+
+        /// <summary>
+        /// Время ожидания записи в порт.
+        /// </summary>
+        public int WriteTimeout { get; set; }
+
+        /// <summary>
+        /// ID канала, из которого только и будет считывать команды бот.
+        /// </summary>
+        public int RequiredChannelId { get; set; }
+    }
+}
